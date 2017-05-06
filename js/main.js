5aabf78e35da16a40c4cb3b9b078354706da6884
Malia Waddell <malia.waddell@gmail.com>
2017-05-06 11:28:01 -0700

    Add scrolling background

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var game = new Phaser.Game(1000,850, Phaser.AUTO, '');
 var score = new ScoreKeeper();
+var background;
 
+
+<<<<<<< HEAD
 function preload() {
     game.load.image('SkyNebula1', 'Assets/background/skyNebula_256LH.png');
     game.load.image('SkyNebula2', 'Assets/background/skyNebula_LH.png');
@@ -8,15 +11,23 @@ function preload() {
     game.load.image('PlayerShip', 'Assets/PlayerShip/ship_1.png');
   
 }
+=======
+console.log(score.getScore());
+>>>>>>> Add scrolling background
 
 var mainState ={
+   
     create: function(){
-        // all items needed at game creation
+        
+        // Add background
+        background = game.add.tileSprite(0, 0, game.width, game.height, 'skyNebula1');
     },
      
      
     update: function(){
-        // all items needed during game loop
+
+        // Scroll background
+        background.tilePosition.y += 0.5;
     }
     
 };
