d59ed6e8fa0dc322324864ee20318e0f3300993f
John Jenson <JRRJ@users.noreply.github.com>
2017-05-06 11:32:53 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ var game = new Phaser.Game(1000,850, Phaser.AUTO, '');
 var score = new ScoreKeeper();
 var background;
 
-
-<<<<<<< HEAD
 function preload() {
     game.load.image('SkyNebula1', 'Assets/background/skyNebula_256LH.png');
     game.load.image('SkyNebula2', 'Assets/background/skyNebula_LH.png');
@@ -11,9 +9,6 @@ function preload() {
     game.load.image('PlayerShip', 'Assets/PlayerShip/ship_1.png');
   
 }
-=======
-console.log(score.getScore());
->>>>>>> Add scrolling background
 
 var mainState ={
    
