901b24a1ef8dda2506661066b322f290c43eecab
matt-codeslo <matt@codeslo.com>
2017-05-06 09:59:19 -0700

    matts initial commit game object created index html set added stylesheet

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var game = new Phaser.Game(1024, 1024, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+    
+}
+
+var mainState ={
+    create: function(){
+        // all items needed at game creation
+    },
+     
+     
+    update: function(){
+        // all items needed during game loop
+    }
+    
+};
+
