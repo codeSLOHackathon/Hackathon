6ca95182b839b9bc18d6ac5a4905b534755ab35f
Camila Souza <Camila@pcp046741pcs.wireless.calpoly.edu>
2017-05-06 13:54:17 -0700

    audio

--- js/main.js
+++ js/main.js
@@ -18,47 +18,47 @@ var mainState ={
     game.load.image('playerGreenShip', 'Assets/PlayerShip/playerShip.png');
     game.load.image('playerBlueShip', 'Assets/PlayerShip/spikedShipBlue.png');
     game.load.image('playerBlt','Assets/Effects/BuletPlr.png');
-    game.load.image('enemyDrone','Assests/Enemies/EnemyShipDrone.png');
+    game.load.image('enemyDrone','Assets/Enemies/EnemyShipDrone.png');
     game.load.image('enemyHunter','Assets/Enemies/EnemyShipHunter.png');
 
-    game.load.audio('laser1', 'soundFx/Laser/Laser_00.wav');
-    game.load.audio('laser2', 'soundFx/Laser/Laser_01.wav');
-    game.load.audio('laser3', 'soundFx/Laser/Laser_02.wav');
-    game.load.audio('laser4', 'soundFx/Laser/Laser_03.wav');
-    game.load.audio('laser5', 'soundFx/Laser/Laser_04.wav');
-    game.load.audio('laser6', 'soundFx/Laser/Laser_05.wav');
-    game.load.audio('laser7', 'soundFx/Laser/Laser_06.wav');
-    game.load.audio('laser8', 'soundFx/Laser/Laser_07.wav');
-    game.load.audio('laser9', 'soundFx/Laser/Laser_08.wav');
-    game.load.audio('laser9', 'soundFx/Laser/Laser_09.wav');
+    game.load.audio('laser1', 'Assets/soundFx/Laser/Laser_00.wav');
+    game.load.audio('laser2', 'Assets/soundFx/Laser/Laser_01.wav');
+    game.load.audio('laser3', 'Assets/soundFx/Laser/Laser_02.wav');
+    game.load.audio('laser4', 'Assets/soundFx/Laser/Laser_03.wav');
+    game.load.audio('laser5', 'Assets/soundFx/Laser/Laser_04.wav');
+    game.load.audio('laser6', 'Assets/soundFx/Laser/Laser_05.wav');
+    game.load.audio('laser7', 'Assets/soundFx/Laser/Laser_06.wav');
+    game.load.audio('laser8', 'Assets/soundFx/Laser/Laser_07.wav');
+    game.load.audio('laser9', 'Assets/soundFx/Laser/Laser_08.wav');
+    game.load.audio('laser9', 'Assets/soundFx/Laser/Laser_09.wav');
 
-    game.load.audio('alarmLoop', 'soundFx/Laser/Alarm_Loop_00.wav');
-    game.load.audio('alarmLoop1', 'soundFx/Laser/Alarm_Loop_01.wav');
+    game.load.audio('alarmLoop', 'Assets/soundFx/Alarm_Loop_00.wav');
+    game.load.audio('alarmLoop1', 'Assets/soundFx/Alarm_Loop_01.wav');
 
-    game.load.audio('alienLanguage', 'soundFx/Laser/Alien_Language_00.wav');
-    game.load.audio('ambience1', 'soundFx/Laser/Ambience_AlienHive_00.wav');
-    game.load.audio('ambience2', 'soundFx/Laser/Ambience_AlienPlanet_00.wav');
-    game.load.audio('ambience3', 'soundFx/Laser/Ambience_BlackHole_00.wav');
-    game.load.audio('ambience4', 'soundFx/Laser/Ambience_Space_00.wav');
-    game.load.audio('ambienceAchievement', 'soundFx/Laser/Ambience_Achievement_00.wav');
-    game.load.audio('jingleLose', 'soundFx/Laser/Jingle_Lose_00.wav');
-    game.load.audio('jingleWin', 'soundFx/Laser/Jingle_Win_00.wav');
-    game.load.audio('jingleWi1', 'soundFx/Laser/Jingle_Win_01.wav');
+    game.load.audio('alienLanguage', 'Assets/soundFx/Alien_Language_00.wav');
+    game.load.audio('ambience1', 'Assets/soundFx/Ambience_AlienHive_00.wav');
+    game.load.audio('ambience2', 'Assets/soundFx/Ambience_AlienPlanet_00.wav');
+    game.load.audio('ambience3', 'Assets/soundFx/Ambience_BlackHole_00.wav');
+    game.load.audio('ambience4', 'Assets/soundFx/Ambience_Space_00.wav');
+    game.load.audio('ambienceAchievement', 'Assets/soundFx/Jingle_Achievement_00.wav');
+    game.load.audio('jingleLose', 'Assets/soundFx/Jingle_Lose_00.wav');
+    game.load.audio('jingleWin', 'Assets/soundFx/Jingle_Win_00.wav');
+    game.load.audio('jingleWi1', 'Assets/soundFx/Jingle_Win_01.wav');
 
-    game.load.audio('menuSelect', 'soundFx/Laser/Menu_Select_00.wav');
-    game.load.audio('menuSelect1', 'soundFx/Laser/Menu_Select_01.wav');
+    game.load.audio('menuSelect', 'Assets/soundFx/Menu_Select_00.wav');
+    game.load.audio('menuSelect1', 'Assets/soundFx/Menu_Select_01.wav');
 
-    game.load.audio('robotActivated', 'soundFx/Laser/Robot_Activated_00.wav');
-    game.load.audio('robotTalk', 'soundFx/Laser/Robot_Talk_01.wav');
-    game.load.audio('robotTalk2', 'soundFx/Laser/Robot_Talk_02.wav');
+    game.load.audio('robotActivated', 'Assets/soundFx/Robot_Activated_00.wav');
+    game.load.audio('robotTalk', 'Assets/soundFx/Robot_Talk_01.wav');
+    game.load.audio('robotTalk2', 'Assets/soundFx/Robot_Talk_02.wav');
 
-    game.load.audio('engineLarge', 'soundFx/Laser/SpaceShip_Engine_Large_Loop_00.wav');
-    game.load.audio('engineMedium', 'soundFx/Laser/SpaceShip_Engine_Medium_Loop_00.wav');
-    game.load.audio('engineSmall', 'soundFx/Laser/SpaceShip_Engine_Small_Loop_00.wav');
+    game.load.audio('engineLarge', 'Assets/soundFx/SpaceShip_Engine_Large_Loop_00.wav');
+    game.load.audio('engineMedium', 'Assets/soundFx/SpaceShip_Engine_Medium_Loop_00.wav');
+    game.load.audio('engineSmall', 'Assets/soundFx/SpaceShip_Engine_Small_Loop_00.wav');
 
-    game.load.audio('warpDrive', 'soundFx/Laser/WarpDrive_00.wav');
-    game.load.audio('warpDrive1', 'soundFx/Laser/WarpDrive_01.wav');
-    game.load.audio('warpDrive2', 'soundFx/Laser/WarpDrive_02.wav');
+    game.load.audio('warpDrive', 'Assets/soundFx/WarpDrive_00.wav');
+    game.load.audio('warpDrive1', 'Assets/soundFx/WarpDrive_01.wav');
+    game.load.audio('warpDrive2', 'Assets/soundFx/WarpDrive_02.wav');
 },
     
     create: function(){
