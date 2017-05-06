40c9a39620df12beddb6cae06e534f109c80871c
Camila Souza <Camila@pcp046741pcs.wireless.calpoly.edu>
2017-05-06 12:20:11 -0700

    declared the score text. added the syntax to the scoreText

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var game = new Phaser.Game(1000,850, Phaser.AUTO, '');
 var score = new ScoreKeeper();
 var background;
+var scoreText;
 
 
 var mainState ={
@@ -10,11 +11,13 @@ var mainState ={
     game.load.image('enemyShip1', 'Assets/Enemies/largeShip1.png');
     game.load.image('enemyShip2', 'Assets/Enemies/ship2.png');
     game.load.image('playerGreenShip', 'Assets/PlayerShip/ship5.png');
-     game.load.image('playerBlueShip', 'Assets/PlayerShip/spikedShipBlue.png');
+    game.load.image('playerBlueShip', 'Assets/PlayerShip/spikedShipBlue.png');
+    game.load.audio('explosion', 'assets/audio/SoundEffects/explosion.mp3');
     },
-
+    
     create: function(){
         // all items needed at game creation
+        scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', font: "Bauhaus 93",fill: '#000' });
         
         // Add background
         background = game.add.tileSprite(0, 0, game.width, game.height, 'skyNebula1');
