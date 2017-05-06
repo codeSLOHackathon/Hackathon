ef907623cd271b53a2ac5200af8efc0311fd9344
Camila Souza <Camila@pcp046741pcs.wireless.calpoly.edu>
2017-05-06 11:34:19 -0700

    updated the images. added the images to the preload function. moved the preload to var mainState

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 var game = new Phaser.Game(1000,850, Phaser.AUTO, '');
 var score = new ScoreKeeper();
 
-function preload() {
+
+var mainState ={
+
+    preload: function(){
     game.load.image('skyNebula1', 'Assets/Background/skyNebula1.png');
     game.load.image('skyNebula2', 'Assets/Background/skyNebula2.png');
     game.load.image('enemyShip', 'Assets/Enemies/largeShip1.png');
     game.load.image('enemyShip', 'Assets/Enemies/ship2.png');
     game.load.image('playerShip', 'Assets/PlayerShip/ship5.png');
      game.load.image('playerShip', 'Assets/PlayerShip/spikedShipBlue.png');
-}
-
-var mainState ={
+    }
     create: function(){
         // all items needed at game creation
     },
